bf1959ff23a84590fce98a939fd755fa10509d91
a-benini <attilio.benini@gmx.net>
2021-04-13 13:37:05 +0200

    test tibble as argument data

--- tests/testthat/test-corr_mat_data.R
+++ tests/testthat/test-corr_mat_data.R
@@ -4,6 +4,12 @@ test_that("mdepriv: argument data", {
     corr_mat(as.matrix(simul_data), c("y1", "y2", "y5"))
   )
 
+  library(tibble)
+  expect_equal(
+    corr_mat(simul_data, c("y1", "y2", "y5")),
+    corr_mat(as_tibble(simul_data), c("y1", "y2", "y5"))
+  )
+
   expect_error(
     corr_mat("simul_data", c("y1", "y2", "y5")),
     "The argument 'data' is neither of the class \"data.frame\" nor of the class \"matrix\"."
--- tests/testthat/test-mdepriv_data.R
+++ tests/testthat/test-mdepriv_data.R
@@ -4,6 +4,12 @@ test_that("mdepriv: argument data", {
     mdepriv(as.matrix(simul_data), c("y1", "y2", "y5"))
   )
 
+  library(tibble)
+  expect_equal(
+    mdepriv(simul_data, c("y1", "y2", "y5")),
+    mdepriv(as_tibble(simul_data), c("y1", "y2", "y5"))
+  )
+
   expect_error(
     mdepriv("simul_data", c("y1", "y2", "y5")),
     "The argument 'data' is neither of the class \"data.frame\" nor of the class \"matrix\"."
